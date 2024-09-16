4c673e4938b14d046c51db3431f01fa175059374
Dasher <55836007+the1andonlyDasher@users.noreply.github.com>
2024-09-16 19:33:18 +0200

    changed layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,22 +7,12 @@ import { prisma } from '@/lib/prisma';
 
 
 export default async function Home() {
-  const supabase = createServerComponentClient({ cookies });
-  const { data } = await supabase.auth.getSession();
-  // if (!data.session?.user) {
-  //   redirect("/login");
-  // }
 
 
   return (
 
     <section>
-      {/* <h1 className="text-2xl text-center mb-8">Protected page</h1> */}
-      {/* <pre>{JSON.stringify({ session: data.session }, null, 4)}</pre> */}
-      {/* <div className='w-full flex flex-row justify-end gap-6 mb-auto'>
-        <Link className='btn__alt' href="/login">Login</Link>
-        <Link className="btn__alt" href="/registrieren">Registrieren</Link>
-      </div> */}
+
     </section>
 
 
